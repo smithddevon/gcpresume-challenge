39ce52ecab369b333a5a2a9ef570d9e3bbf0cac6
Devon Smith <dsmith418@icloud.com>
2024-11-11 15:00:16 -0500

    Removed pubsub trigger/added cloud storage trigger

--- infra/main.tf
+++ infra/main.tf
@@ -9,21 +9,6 @@ resource "google_storage_bucket" "resume_bucket" {
   location = "us-central1"
 }
 
-# Pub/Sub topic for Cloud Storage updates
-resource "google_pubsub_topic" "update_resume_topic" {
-  name     = "update-resume-topic"
-  project  = "gcp-resume-challenge-083124"
-  location = "us-central1"
-}
-
-# Pub/Sub subscription for Cloud Function trigger
-resource "google_pubsub_subscription" "update_resume_subscription" {
-  name  = "update-resume-subscription"
-  topic = google_pubsub_topic.update_resume_topic.name
-  project = "gcp-resume-challenge-083124"
-  ack_deadline_seconds = 20
-}
-
 # Cloud Function to retrieve resume data from Firestore (HTTP-triggered)
 resource "google_cloudfunctions_function" "resume_function" {
   name                  = "get_resume"
@@ -45,7 +30,7 @@ resource "google_cloudfunctions_function" "update_firestore_function" {
   runtime               = "python39"
   entry_point           = "upload_to_firestore"
   source_archive_bucket = google_storage_bucket.resume_bucket.name
-  source_archive_object = "source.zip"
+  source_archive_object = google_storage_bucket_object.source_archive.name
   available_memory_mb = 256
   environment_variables = {
     FIRESTORE_PROJECT = "gcp-resume-challenge-083124"
@@ -53,15 +38,18 @@ resource "google_cloudfunctions_function" "update_firestore_function" {
   service_account_email = "cloud-storage-service@gcp-resume-challenge-083124.iam.gserviceaccount.com"
   
   event_trigger {
-    event_type = "google.cloud.pubsub.topic.v1.messagePublished"
-    resource   = google_pubsub_topic.update_resume_topic.name
+    event_type = "google.cloud.storage.object.v1.finalized"
+    resource   = google_storage_bucket.resume_bucket.name
   }
 }
 
-# Cloud Storage notification to Pub/Sub topic
-resource "google_storage_notification" "resume_notification" {
-  bucket         = google_storage_bucket.resume_bucket.name
-  payload_format = "JSON_API_V1"
-  topic          = google_pubsub_topic.update_resume_topic.name
-  event_types    = ["OBJECT_FINALIZE", "OBJECT_UPDATE"]
+# Firestore document
+resource "google_firestore_document" "resume_document" {
+  collection  = "(default)"
+  document_id = "resume-db"
+  fields      = {
+    title = {
+      string_value = "Resume Data"
+    }
+  }
 }
